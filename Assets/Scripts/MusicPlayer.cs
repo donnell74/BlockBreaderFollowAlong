cb06187e95829ec23ab1ac1a2c7be0a9796edf86
Greg Donnell <gregory.donnell74@gmail.com>
2018-05-25 11:31:22 -0600

    First commit, up to end of S7 L81

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+    private static MusicPlayer musicPlayerInstance = null;
+
+	// Use this for initialization
+	void Start () {
+        if (musicPlayerInstance != null) {
+            Destroy(gameObject);
+            Debug.Log("Duplicate music player destroyed!");
+        } else {
+            musicPlayerInstance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
