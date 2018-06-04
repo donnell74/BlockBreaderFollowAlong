acd5bb6f93ef6bcc73a7425951dc25932358e3a4
Greg Donnell <gregory.donnell74@gmail.com>
2018-06-04 05:27:02 -0600

    End of S6 L79

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+
+    public int maxHits;
+    private int timesHit;
+
+	// Use this for initialization
+	void Start () {
+        timesHit = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        timesHit += 1;
+        Debug.Log("Times hit: " + timesHit);
+
+        if (timesHit >= maxHits) {
+            Destroy(this);
+        }
+    }
+}
