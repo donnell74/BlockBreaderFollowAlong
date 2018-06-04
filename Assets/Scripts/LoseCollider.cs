8339818fcd1ec328bfe44cdedf3201f39a21b5f9
Greg Donnell <gregory.donnell74@gmail.com>
2018-06-03 19:12:32 -0600

    End of S7 L73

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+
+    public LevelManager levelManager;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log("Trigger");
+        levelManager.LoadLevel("Lose");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        Debug.Log("Collision");
+        levelManager.LoadLevel("Lose");
+    }
+}
