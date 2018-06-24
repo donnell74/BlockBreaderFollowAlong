160da4e6bc52029d250ac76a1d2753df5eefb196
Greg Donnell <gregory.donnell74@gmail.com>
2018-06-23 19:10:03 -0600

    End of S7 L78

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -29,8 +29,11 @@ public class Ball : MonoBehaviour {
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
+        Vector2 tweak = new Vector2(Random.Range(0f, 0.2f), Random.Range(0f, 0.2f));
+
         if (ballLaunched) {
-            GetComponent<AudioSource>().Play();
+            this.GetComponent<AudioSource>().Play();
+            this.GetComponent<Rigidbody2D>().velocity += tweak;
         }
     }
 }
